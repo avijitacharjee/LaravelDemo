5426f5b77df2bb93062e796c4fbd2ee6651f525c
Avijit Acharjee <avijitach@gmail.com>
2020-07-01 16:56:51 +0600

    store and retrieve image

--- app/Gallery.php
+++ app/Gallery.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
-    //
+    protected $table='gallery';
+    protected $fillable=['id','filename'];
 }
--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,24 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Gallery;
 
 class GalleryController extends Controller
 {
     public function storeImages(Request $request)
     {
+        $data=array();
         if($request->hasfile('filename'))
         {
             foreach($request->file('filename') as $image)
             {
                 $name=$image->getClientOriginalName();
                 $image->move(public_path().'/gallery/images',$name);
-                $data[]= $name;
+                array_push($data,$name);
             }
         }
         $images = new Gallery();
         $images->filename=json_encode($data);
-        $images->save();
-        return "success";
+        if($images->save())
+        {
+            return json_encode([
+                'data'=>$data,
+                'message'=>'saved'
+            ]);
+        }
+        else
+        {
+            return json_encode([
+                'data'=>null,
+                'message'=>'Failed to save'
+            ]);
+        }
+        
+    }
+    public function show()
+    {
+        $gallery = new Gallery();
+        $data=$gallery->all();
+        return json_encode([
+            'data'=>$data,
+            'message'=>'successful'
+        ]);
     }
      
 }
